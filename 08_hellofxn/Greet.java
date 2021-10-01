3fb4547779afb5264a0785c00ca20450f97ea08c
Xinqing <xlin30@stuy.edu>
2021-09-30 20:00:53 -0400

    08

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,13 @@
+public class Greet {
+	public static void main(String[] args){
+		System.out.println("No hablo queso");
+		greet("Success");
+	}
+	
+	public static void greet(String x){
+		System.out.println(x);
+}
+}
+/*
+What does hardcoded mean? Persons of interest?
+*/
