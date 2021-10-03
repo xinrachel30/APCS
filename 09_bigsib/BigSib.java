477421601e3414fdee6a3dd9f35fb38cadcf5795
Xinqing Lin <78173064+xinrachel30@users.noreply.github.com>
2021-10-03 16:15:06 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,5 +1,5 @@
 public class BigSib {
     public static void greet(String x) {
-        System.out.println("Good morning, " + x + ". How are you?")
+        System.out.println("Good morning, " + x + ". How are you?");
     }
 }
