d7b02cb418f0aed147cc9884fa79c26fb7c2ddc3
Xinqing Lin <78173064+xinrachel30@users.noreply.github.com>
2021-10-03 16:38:33 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,3 +1,20 @@
+/* 
+MAX Swag (Xinqing Lin (Horse), Anjini Katari (Watson), and Michael Kamela (no duck yet))
+APCS
+HW09 -- Using methods across separate Java files
+2021-10-03
+*/
+
+/*
+DISCOVERIES:
+- If you forget to make a java class, it will give you the error message: "cannot find symbol".
+- You can use a method of one Java file in another Java file (as shown below) if they're in the same folder.
+- You can write (and use) a class without a main method. 
+
+UNRESOLVED QUESTIONS: 
+- What's the difference between a private class and a public class?
+*/
+
 public class BigSib {
     public static void greet(String x) {
         System.out.println("Good morning, " + x + ". How are you?");
