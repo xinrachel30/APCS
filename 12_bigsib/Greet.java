12285a92885f0547dbf1fad7ff5ffc780460f7f1
Xinqing Lin <78173064+xinrachel30@users.noreply.github.com>
2021-10-05 20:37:25 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -1,3 +1,19 @@
+/* 
+MAX Swag (Xinqing Lin (Horse), Anjini Katari (Watson), and Michael Kamela (no duck yet))
+APCS
+HW12 -- Using instance variables
+2021-10-05
+*/
+
+/*
+DISCO:
+- You can create a (instance) variable outside of a method that can be used by all methods. 
+- In order to be able to use an instance variable, you have to make the methods nonstatic by deleting the static after each public. Otherwise, you get the error message: "error: non-static variable hellomsg cannot be referenced from a static context"
+
+QCC:
+- What is the difference between static and nonstatic?
+*/
+
 public class Greet {
   public static void main( String[] args ) {
     String greeting;
