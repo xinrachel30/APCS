3c8e8866121a627697f78a7c9d9026cc4e2ff09e
xinrachel30 <xlin30@stuy.edu>
2021-10-05 20:23:59 -0400

    hw12

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,9 @@
+public class BigSib {
+    public String helloMsg;
+    public String greet(String x) {
+        return helloMsg + " " + x;
+    }
+    public void setHelloMsg(String x) {
+        helloMsg = x;
+    }
+}
