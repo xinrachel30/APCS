e48116ffabeff2cf0972fcef535c989669f48cc1
xinrachel30 <xlin30@stuy.edu>
2021-10-06 21:38:38 -0400

    hw13

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -1,7 +1,23 @@
+/*
+HorseSoup (Xinqing Lin (Horse) and Paul Serbanescu (Soup))
+APCS
+HW13 -- Where do BigSibs Come From? / Using Constructors
+2021-10-06
+*/
+
+/*
+DISCO:
+- Using a constructor eliminates the need for setHelloMsg.
+- The constructor must have the class name after the protection type.
+
+QCC:
+- None
+*/
+
 public class BigSib {
     public String helloMsg;
-    public BigSib(){
-      helloMsg = "Word up";
+    public BigSib(String s){
+      helloMsg = s;
     }
     public String greet(String x) {
         return helloMsg + " " + x;
--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -1,8 +1,24 @@
+/* 
+HorseSoup (Xinqing Lin (Horse) and Paul Serbanescu (Soup))
+APCS
+HW13 -- Where do BigSibs Come From? / Using Constructors
+2021-10-06
+*/
+
+/*
+DISCO:
+- Using a constructor eliminates the need for setHelloMsg.
+- The constructor must have the class name after the protection type.
+
+QCC:
+- None
+*/
+
 public class Greet {
   public static void main( String[] args ) {
     String greeting;
 
-    BigSib richard = new BigSib();
+    BigSib richard = new BigSib("Word up");
 
     greeting = richard.greet("freshman");
     System.out.println(greeting);
