a9413d0b559381f323a8a5fab4304bac14156bc7
xinrachel30 <xlin30@stuy.edu>
2021-10-16 18:56:07 -0400

    questions

--- 18_BankAccount/BankAccount.java
+++ 18_BankAccount/BankAccount.java
@@ -1,7 +1,21 @@
-/*The Cats: Perry Huang, Xinqing Lin, Faiyaz Rafee
+/*
+The Cats: Perry Huang, Xinqing Lin, Faiyaz Rafee
 APCS
-HW18 -- <CPA-One/Bank Account Java/Summary>
+HW18 -- <CPA-One/Codify BankAccount.java/Making methods inside BankAccount to view details, withdraw and deposit money, and make an account>
 2021-08-15
+*/
+
+/*
+DISCO:
+We discovered that you should use a period to invoke a method with an object - not accDetails(PerryThePlatypus), but PerryThePlatypus.accDetails(params).
+
+QCC:
+How does PerryThePlatypus.accDetails(params) work?
+
+Q2: Before we write a constructor, we can make an instance variable using "BankAccount Perry = new BankAccount();"
+We know that there is a default constructor because we can successfully make an instance without any arguments.
+
+Q3: You can get the string representation using "System.out.println(object);
 */
 
 public class BankAccount{
