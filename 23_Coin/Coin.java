59908768ab6607f67ef20ec29fd83a5d7d975665
xinrachel30 <xlin30@stuy.edu>
2021-10-23 23:22:25 -0400

    hw23

---
+++ 23_Coin/Coin.java
@@ -0,0 +1,187 @@
+/***
+ TNPG: Blue Sushi (Lawrence Joa and Xinqing Lin)
+ APCS
+ HW23 -- What does Equality Look like?/Using Random()
+ 21-10-23
+ *  class Coin
+ *  by Clyde "Thluffy" Sinclair
+ *  SKELETON
+
+ DISCO:
+ Math.random() provides a value in the interval [0.0, 1.0). We can use Math.random() in problems involving probability.
+
+ QCC:
+ How should we set the value for bias?
+ ***/
+
+public class Coin {
+  private double value;
+  private String upFace;
+  private String name;
+  private int flipCtr;
+  private int headsCtr;
+  private int tailsCtr;
+  private double bias;
+  //attributes aka instance vars
+
+
+
+  /***
+   *  Coin() -- default constuctor
+   *  precond:
+   *  postcond:
+   ***/
+  public Coin() {
+  	upFace = "heads";
+  }
+
+
+  /***
+      Coin(String) -- overloaded constructor
+      precond: input is one of
+      "penny",
+      "nickel",
+      "dime",
+      "quarter",
+      "half dollar",
+      "dollar"
+      postcond:
+  ***/
+  public Coin( String s ) {
+  	name = s;
+    upFace = "heads";
+  }
+
+
+  /***
+      Coin(String,String) --
+      precond:
+      postcond:
+  ***/
+  public Coin( String s, String nowFace ) {
+  	name = s;
+  	assignValue(name);
+  	upFace = nowFace;
+  }
+
+
+  // Accessors...
+  // ----------------------------
+  public String getUpFace() {
+  	return upFace;
+  }
+
+  public int getFlipCtr() {
+	return flipCtr;
+  }
+
+  public double getValue() {
+	return value;
+  }
+
+  public int getHeadsCtr() {
+	return headsCtr;
+  }
+
+  public int getTailsCtr() {
+	return tailsCtr;
+  }
+  // ----------------------------
+
+
+  /***
+   * assignValue() -- set a Coin's monetary value based on its name
+   * precond:  input String is a valid coin name ("penny", "nickel", etc.)
+   * postcond: instvar value gets appropriate value
+   * Returns value assigned.
+   ***/
+  private double assignValue( String s ) {
+	if(s=="penny"){
+		value=0.01;
+	}
+	if(s=="nickel"){
+		value=0.05;
+	}
+	if(s=="dime"){
+		value=0.10;
+	}
+	if(s=="quarter"){
+		value = 0.25;
+	}
+	if(s=="half dollar"){
+		value = 0.50;
+	}
+	if(s=="dollar"){
+		value = 1.00;
+	}
+	return value;
+  }
+
+
+  /***
+      reset() -- initialize a Coin
+      precond:  s is "heads" or "tails", 0.0 <= d <= 1.0
+      postcond: Coin's attribs reset to starting vals
+  ***/
+  public void reset( String s, double d ) {
+		upFace = s;
+		bias = 0.5;
+    flipCtr = 0;
+    headsCtr = 0;
+    tailsCtr = 0;
+  }
+
+
+  /***
+   * String flip() -- simulates a Coin flip
+   * precond:  bias is a double on interval [0.0,1.0]
+   * (1.0 indicates always heads, 0.0 always tails)
+   * postcond: upFace updated to reflect result of flip.
+   * flipCtr incremented by 1.
+   * Either headsCtr or tailsCtr incremented by 1, as approp.
+   * Returns "heads" or "tails"
+   ***/
+  public String flip() {
+  	double result = Math.random();
+    System.out.println(bias);
+    System.out.println(result);
+    flipCtr ++;
+    if (result >= bias) {
+      tailsCtr ++;
+      upFace = "tails";
+      return upFace;
+    }
+    else {
+      headsCtr ++;
+      upFace = "heads";
+      return upFace;
+  	}
+  }
+
+
+  /***
+   * boolean equals(Coin) -- checks to see if 2 coins have same face up
+   * precond: other is not null
+   * postcond: Returns true if both coins showing heads
+   * or both showing tails. False otherwise.
+   ***/
+  public boolean equals( Coin other ) {
+    if (other.upFace == upFace) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+
+  /***
+   * String toString() -- overrides toString() provided by Java
+   * precond: n/a
+   * postcond: Return String comprised of name and current face
+   ***/
+  public String toString() {
+		return name + " -- " + upFace;
+  }
+
+}//end class
