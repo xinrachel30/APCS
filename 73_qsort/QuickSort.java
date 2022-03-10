61461acd6e51f643601e4b1688296df1cd3c072a
xinrachel30 <xlin30@stuy.edu>
2022-03-10 18:53:20 -0500

    + library

--- 73_qsort/QuickSort.java
+++ 73_qsort/QuickSort.java
@@ -69,19 +69,57 @@ public class QuickSort
    * void qsort(int[])
    * @param d -- array of ints to be sorted in place
    */
-  public static void qsort( int[] d )
+  //  public static void qsort( int[] d )               OUR ORIGINAL CODE
+  // {
+  //   QuickSelect selector = new QuickSelect();
+  //   //Partition partt = new Partition();
+  //   for (int i = 1; i < d.length; i += 2) {
+  //     selector.quickSelect(d, 0, d.length - 1, i);
+  //   }
+  // }
+
+   public static void qsort( int[] d )                 // CODE FROM LIBRARY
+   {
+     qSortHelpr(d, 0, d.length-1);
+   }
+
+
+   /***
+    * void qSortHelpr(int[], int, int)
+    * @param arr -- array of ints to be sorted in place
+    * @param loPos -- lower bound for partition range
+    * @param hiPos -- upper bound for partition range
+    ***/
+
+
+   public static void qSortHelpr( int[] arr, int loPos, int hiPos ) {   // CODE FROM LIBRARY
+     if ( loPos < hiPos ) {
+       int pvtPos = partition(arr, loPos, hiPos);
+       System.out.println("this is one");
+       qSortHelpr(arr, loPos, pvtPos-1);
+       qSortHelpr(arr, pvtPos+1, hiPos);
+     }
+   }
+
+   public static int partition( int arr[], int loPos, int hiPos ) // CODE FROM LIBRARY
   {
-    QuickSelect selector = new QuickSelect();
-    //Partition partt = new Partition();
-    for (int i = 1; i < d.length; i += 2) {
-      selector.quickSelect(d, 0, d.length - 1, i);
+    int pvtPos = loPos;
+
+    int pvtVal = arr[pvtPos];
+
+    swap( pvtPos, hiPos, arr);
+
+    int s = loPos;
+
+    for( int i = loPos; i < hiPos; i++ ) {
+      if ( arr[i] <= pvtVal ) {
+        swap( i, s, arr );
+        s++;
+      }
     }
-  }
+    swap(s, hiPos, arr);
 
-  //you may need a helper method...
-
-  public static void helper( int[] d) {
-
+    return s;
   }
 
 
@@ -90,7 +128,7 @@ public class QuickSort
   {
 
     //get-it-up-and-running, static test case:
-    int [] arr1 = {7,1,5,12,3};
+    int [] arr1 = {1, 2, 5, 4, 3};
     System.out.println("\narr1 init'd to: " );
     printArr(arr1);
     qsort( arr1 );
@@ -98,43 +136,43 @@ public class QuickSort
     printArr(arr1);
 
 
-    // randomly-generated arrays of n distinct vals
-    int[] arrN = new int[10];
-    for( int i = 0; i < arrN.length; i++ )
-    arrN[i] = i;
-    System.out.println("\narrN init'd to: " );
-    printArr(arrN);
-    shuffle(arrN);
-    System.out.println("arrN post-shuffle: " );
-    printArr(arrN);
-    qsort( arrN );
-    System.out.println("arrN after sort: " );
-    printArr(arrN);
-      /*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-
-
-    //get-it-up-and-running, static test case w/ dupes:
-    int [] arr2 = {7,1,5,12,3,7};
-    System.out.println("\narr2 init'd to: " );
-    printArr(arr2);
-    qsort( arr2 );
-    System.out.println("arr2 after qsort: " );
-    printArr(arr2);
-
-    // arrays of randomly generated ints
-    int[] arrMatey = new int[20];
-    for( int i = 0; i < arrMatey.length; i++ )
-    arrMatey[i] = (int)( 48 * Math.random() );
-    System.out.println("\narrMatey init'd to: " );
-    printArr(arrMatey);
-    shuffle(arrMatey);
-    System.out.println("arrMatey post-shuffle: " );
-    printArr(arrMatey);
-    qsort( arrMatey );
-    System.out.println("arrMatey after sort: " );
-    printArr(arrMatey);
+    // // randomly-generated arrays of n distinct vals
+    // int[] arrN = new int[10];
+    // for( int i = 0; i < arrN.length; i++ )
+    // arrN[i] = i;
+    // System.out.println("\narrN init'd to: " );
+    // printArr(arrN);
+    // shuffle(arrN);
+    // System.out.println("arrN post-shuffle: " );
+    // printArr(arrN);
+    // qsort( arrN );
+    // System.out.println("arrN after sort: " );
+    // printArr(arrN);
+    //   /*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    //
+    //
+    //
+    // //get-it-up-and-running, static test case w/ dupes:
+    // int [] arr2 = {7,1,5,12,3,7};
+    // System.out.println("\narr2 init'd to: " );
+    // printArr(arr2);
+    // qsort( arr2 );
+    // System.out.println("arr2 after qsort: " );
+    // printArr(arr2);
+    //
+    // // arrays of randomly generated ints
+    // int[] arrMatey = new int[20];
+    // for( int i = 0; i < arrMatey.length; i++ )
+    // arrMatey[i] = (int)( 48 * Math.random() );
+    // System.out.println("\narrMatey init'd to: " );
+    // printArr(arrMatey);
+    // shuffle(arrMatey);
+    // System.out.println("arrMatey post-shuffle: " );
+    // printArr(arrMatey);
+    // qsort( arrMatey );
+    // System.out.println("arrMatey after sort: " );
+    // printArr(arrMatey);
 /*~~~~s~l~i~d~e~~~m~e~~~d~o~w~n~~~~~~~~~~~~~~~~~~~~ (C-k, C-k, C-y)
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
